6d9c7f7106613ce1f851de43ca8a689d1ea24793
shiqiang <shiqiang@gmail.com>
2020-04-20 17:39:50 +0800

    Tab功能

--- app/src/main/java/com/example/tab/FristFragment.java
+++ app/src/main/java/com/example/tab/FristFragment.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FristFragment extends Fragment {
+    private  Button btn1;
+    private TextView tv1;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_frist, null);
+        btn1=view.findViewById(R.id.btn1);
+        tv1=view.findViewById(R.id.tv1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv1.setText("我更新了");
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/example/tab/TabLayoutMainActivity.java
+++ app/src/main/java/com/example/tab/TabLayoutMainActivity.java
@@ -1,8 +1,6 @@
 package com.example.tab;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -13,14 +11,8 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 
 public class TabLayoutMainActivity extends AppCompatActivity {
-    //未选中的Tab图片
-    private int[] unSelectTabRes = new int[]{R.drawable.home_page
-            , R.drawable.entertainment,R.drawable.game, R.drawable.my};
-    //选中的Tab图片
-    private int[] selectTabRes = new int[]{R.drawable.homepage_press, R.drawable.entertainment_press
-            , R.drawable.game_press, R.drawable.my_press};
     //Tab标题
-    private String[] title = new String[]{"首页", "娱乐", "游戏", "我的"};
+    private String[] title = new String[]{"自动", "手动", "状态", "设置","系统"};
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private TabLayout.Tab tabAtOne;
@@ -31,7 +23,7 @@ public class TabLayoutMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  getSupportActionBar().hide();//隐藏掉整个ActionBar
+       //getSupportActionBar().hide();//隐藏掉整个ActionBar
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar=findViewById(R.id.toolbar);
@@ -39,7 +31,6 @@ public class TabLayoutMainActivity extends AppCompatActivity {
 
         initView();
         initData();
-      //  initListener();
     }
 
     private void initView() {
@@ -50,67 +41,13 @@ public class TabLayoutMainActivity extends AppCompatActivity {
         viewPager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager()));
         //将TabLayout与ViewPager绑定
         tabLayout.setupWithViewPager(viewPager);
-
-      /*  //设置方式一：
-        //获取底部的单个Tab
-        tabAtOne = tabLayout.getTabAt(0);
-        tabAttwo = tabLayout.getTabAt(1);
-        tabAtthree = tabLayout.getTabAt(2);
-        tabAtfour = tabLayout.getTabAt(3);
-
-        //设置Tab图片
-        tabAtOne.setIcon(R.drawable.i8live_menu_home_press);
-        tabAttwo.setIcon(R.drawable.i8live_menu_information_normal);
-        tabAtthree.setIcon(R.drawable.i8live_menu_game_normal);
-        tabAtfour.setIcon(R.drawable.i8live_menu_personl_normal);*/
-
-        //设置方式二：
-//        for (int i = 0; i < title.length; i++) {
-//            if (i == 0) {
-//                tabLayout.getTabAt(0).setIcon(selectTabRes[0]);
-//            } else {
-//                tabLayout.getTabAt(i).setIcon(unSelectTabRes[i]);
-//            }
-//        }
-
     }
 
     private void initData() {
 
     }
 
-//    private void initListener() {
-//        //TabLayout切换时导航栏图片处理
-//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {//选中图片操作
-//
-//                for (int i = 0; i < title.length; i++) {
-//                    if (tab == tabLayout.getTabAt(i)) {
-//                        tabLayout.getTabAt(i).setIcon(selectTabRes[i]);
-//                        viewPager.setCurrentItem(i);
-//                    }
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {//未选中图片操作
-//
-//                for (int i = 0; i < title.length; i++) {
-//                    if (tab == tabLayout.getTabAt(i)) {
-//                        tabLayout.getTabAt(i).setIcon(unSelectTabRes[i]);
-//                    }
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
-//    }
+
 
     //自定义适配器
     public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
@@ -127,6 +64,8 @@ public class TabLayoutMainActivity extends AppCompatActivity {
                 return new ThirtlyFragment();//游戏
             } else if (position == 3) {
                 return new FourthlyFragment();//我的
+            }else if (position==4){
+                return new FifthlyFragment();
             }
             return new FristFragment();//首页
         }
@@ -141,5 +80,8 @@ public class TabLayoutMainActivity extends AppCompatActivity {
             return title[position];
         }
     }
+
+
+
 }
 
